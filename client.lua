6547028642c95c63321c96df68870f73e7035068
Abigail05 <48155330+Abigail05@users.noreply.github.com>
2019-03-04 17:53:24 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,20 +6,42 @@
 -------------------------------------------------------------------------
 --- DO NOT SELL THIS SCRIPT TO ANYONE OR REUSE IT WITHOUT PERMISSION! ---
 
+Rfunction arma(weaponHash)
+    GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(weaponHash), 100, false)
+end
+
+function componenta(weaponHash, component)
+    if HasPedGotWeapon(PlayerPedId(), GetHashKey(weaponHash), false) then
+        GiveWeaponComponentToPed(PlayerPedId(), GetHashKey(weaponHash), GetHashKey(component))
+     end
+end
+
+function sterge(weaponHash)
+    RemoveWeaponFromPed(GetPlayerPed(-1), GetHashKey(weaponHash))
+end
+
 RegisterNetEvent("stergere")
 AddEventHandler("stergere", function()
-    RemoveWeaponFromPed(GetPlayerPed(-1), "weapon_pistol")
-    RemoveWeaponFromPed(GetPlayerPed(-1), "weapon_assaultsmg")
-    RemoveWeaponFromPed(GetPlayerPed(-1), "weapon_carbinerifle")
+    sterge("weapon_carbinerifle")
+    sterge("weapon_combatpistol")
 end)
 
 RegisterNetEvent("arme")
 AddEventHandler("arme", function()
-      GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("weapon_pistol"), 100, false)
-      GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("weapon_assaultsmg"), 100, false)
-      GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("weapon_carbinerifle"), 100, false)
+    arma("weapon_carbinerifle")
+    componenta("weapon_carbinerifle", "COMPONENT_CARBINERIFLE_VARMOD_LUXE")
+        componenta("weapon_carbinerifle", "COMPONENT_CARBINERIFLE_CLIP_02")
+        componenta("weapon_carbinerifle", "COMPONENT_AT_SCOPE_MEDIUM")
+        componenta("weapon_carbinerifle", "COMPONENT_AT_RAILCOVER_01")
+        componenta("weapon_carbinerifle", "COMPONENT_AT_AR_AFGRIP")
+        componenta("weapon_carbinerifle", "COMPONENT_AT_AR_SUPP")
+        componenta("weapon_carbinerifle", "COMPONENT_AT_AR_FLSH")
+    arma("weapon_combatpistol")
+        componenta("weapon_combatpistol", "COMPONENT_COMBATPISTOL_VARMOD_LOWRIDER")
+        componenta("weapon_combatpistol", "COMPONENT_COMBATPISTOL_CLIP_02")
+        componenta("weapon_combatpistol", "COMPONENT_AT_PI_SUPP")
+        componenta("weapon_combatpistol", "COMPONENT_AT_PI_FLSH")
 end)
-
 RegisterNetEvent("notificari")
 AddEventHandler("notificari", function()
     ecran("~y~Weapons", "Be careful how you use them, I'm watching you!", 5500)
